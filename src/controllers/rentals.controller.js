2542f3d74964bb6678cdb507bdb3e2667eade966
anarehder <anapaula.rehder@gmail.com>
2023-05-15 15:33:49 -0300

    Validacoes Return Rental

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -3,8 +3,33 @@ import dayjs from "dayjs";
 
 export async function getRentals(req, res) {
     try {
-        const rentals = await db.query(`SELECT * FROM rentals`);
-        res.send(rentals.rows);
+        const rentals = await db.query(
+            `SELECT rentals.*, games.id AS game_id, games.name AS game_name, customers.id AS customer_id, customers.name AS customer_name
+                FROM rentals 
+                JOIN games ON rentals."gameId" = games.id
+                JOIN customers ON rentals."customerId" = customers.id;`
+        );
+
+        const rentalsComplete = rentals.rows.map((item) => ({
+            id: item.id,
+            customerId: item.customerId,
+            gameId: item.gameId,
+            rentDate: dayjs(item.rentDate).format('YYYY-MM-DD'),
+            daysRented: item.daysRented,
+            returnDate: item.returnDate ? dayjs(item.returnDate).format('YYYY-MM-DD') : null,
+            originalPrice: item.originalPrice,
+            delayFee: item.returnDate ? item.delayFee : null,
+            customer: {
+                id: item.customer_id,
+                name: item.customer_name
+            },
+            game: {
+                id: item.game_id,
+                name: item.game_name
+            }
+        }));
+
+        res.send(rentalsComplete);
     } catch (err) {
         console.error(err);
         res.sendStatus(500);
@@ -28,14 +53,14 @@ export async function insertRental(req, res) {
 
         if (!customerData.rows[0]) {
             res.status(400).send("Esse cliente não está cadastrado");
-        } 
+        }
         const rented = await db.query(`SELECT * FROM rentals WHERE id=$1;`, [gameId]);
         const rentedAmount = rented.rows.length;
         console.log("total alugado", rentedAmount);
-        if (stockTotal>rentedAmount){
+        if (stockTotal > rentedAmount) {
             await db.query(
                 `INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
-                    VALUES ($1, $2, NOW(), $3, null, $4, null);`
+                    VALUES ($1, $2, 'NOW()', $3, null, $4, null);`
                 , [customerId, gameId, daysRented, originalPrice]);
             res.sendStatus(201);
         } else {
@@ -48,34 +73,26 @@ export async function insertRental(req, res) {
 
 export async function finishRental(req, res) {
 
-    try {      
-        //const daysExtra = await db.query(`SELECT DATEDIFF(CURRENT_DATE(), ${expectedReturn});`);
-        //const delayFee = (rental.rows[0].originalPrice / Number(rental.rows[0].daysRented)) * daysExtra;
+    const returnDate = dayjs().format('YYYY-MM-DD');
+    const returnDateForCalc = dayjs().startOf('day');
 
+    try {
         const rentalAspects = await db.query(`SELECT * FROM rentals WHERE id=$1;`, [req.params.id]);
-        //
-        
-
         if (!rentalAspects.rows[0]) {
             res.status(404).send("Esse id não está cadastrado");
         } else if (rentalAspects.rows[0].returnDate !== null) {
             res.status(400).send("O aluguel já está finalizado");
         } else {
-            const daysRented = Number(rentalAspects.rows[0].daysRented);
-            const rentDate = dayjs(rentalAspects.rows[0].rentDate).format('YYYY-MM-DD');
-            const expectedReturn = rentDate + daysRented;
-            const today = dayjs().startOf('day').$d;
-            const returnDate = dayjs(today).format('YYYY-MM-DD');
-            const differenceDays = returnDate - rentDate;
-            console.log(differenceDays)
-            // await db.query(`SELECT DATEADD(day, $1, $2);`,[daysRented, rentDate]);
-            //console.log(expectedReturn)
-/*             await db.query(
-                `INSERT INTO rentals (returnDate, delayFee)
-                    VALUES (NOW(), $1)
-                    WHERE id=$2;`, [delayFee, req.params.id]); */
+            const days = Math.floor((returnDateForCalc.valueOf() - rentalAspects.rows[0].rentDate.valueOf())/86400000);
+            let delayFee;
+            if(days > rentalAspects.rows[0].daysRented){
+                delayFee = (days - rentalAspects.rows[0].daysRented) * (rentalAspects.rows[0].originalPrice / rentalAspects.rows[0].daysRented);
+            }else{
+                delayFee = 0;
+            }    
+            await db.query(`UPDATE rentals SET "delayFee" = $1, "returnDate" = $2 WHERE id = $3;`,[delayFee, returnDate, req.params.id]);
             res.sendStatus(200);
-        } 
+        }
     } catch (err) {
         console.error(err);
         res.sendStatus(500);
@@ -93,7 +110,7 @@ export async function deleteRental(req, res) {
         } else {
             await db.query(`DELETE FROM rentals WHERE id=$1;`, [req.params.id]);
             res.sendStatus(200);
-        }        
+        }
     } catch (err) {
         console.error(err);
         res.sendStatus(500);
